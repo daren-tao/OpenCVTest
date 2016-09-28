89c74e50b65e2e564c3c10df50a44016ccc93286
daren-tao <Mr_Ytao@163.com>
2016-09-28 11:14:44 +0800

    modify opencv module lib and header files

--- algoFunc.h
+++ algoFunc.h
@@ -5,6 +5,10 @@
 #include "opencv2\highgui\highgui.hpp"
 #include "opencv2\imgproc\imgproc.hpp"
 #include "opencv2\contrib\contrib.hpp"
+#include "opencv2\features2d\features2d.hpp"
+#include "opencv2\ml\ml.hpp"
+#include "opencv2\objdetect\objdetect.hpp"
+#include "opencv2\nonfree\nonfree.hpp"
 
 class algoFunc
 {
