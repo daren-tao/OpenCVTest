6515e57718fb0864daf983920ce12cfe0e747833
daren-tao <Mr_Ytao@163.com>
2016-09-28 08:36:06 +0800

    some add in

--- testMain.cpp
+++ testMain.cpp
@@ -22,23 +22,52 @@ int main(int argc, char** argv)
 	cv::namedWindow(grayStr);
 	cv::namedWindow(binStr);
 
+	CvAdaptiveSkinDetector skinDetector;
+
 	cv::Mat orgImg = cv::imread("testImages\\depth_215.bmp");
 	logInfo("origin image channels = %d", orgImg.channels());
-	cv::imshow(orgStr, orgImg);
 
 	cv::Mat grayImg;
 	cv::cvtColor(orgImg, grayImg, CV_RGB2GRAY);
-	logInfo("binary image channels = %d", grayImg.channels());
-	cv::imshow(grayStr, grayImg);
+	logInfo("binary image channels = %d, type = %d", grayImg.channels(), grayImg.type());
+
+	cv::Mat invImg(grayImg.rows, grayImg.cols, grayImg.type());
+	uchar* grayImg_ptr = grayImg.data;
+	uchar* invImg_ptr = invImg.data;
+	for (int i = 0; i < grayImg.rows; i++){
+		for (int j = 0; j < grayImg.cols; j++){
+			invImg_ptr[i*grayImg.cols+j] = grayImg_ptr[i*grayImg.cols+j];
+		}
+	}
+	cv::namedWindow("Inverse image");
+	cv::imshow("Inverse image", invImg);
+
 
 	cv::Mat binImg;
 //	cv::adaptiveThreshold(grayImg, binImg, 180, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 21, 15);
 	cv::threshold(grayImg, binImg, 230, 250, cv::THRESH_BINARY);
+
+	cv::erode(binImg, binImg, cv::Mat(5, 5, CV_8U), cv::Point(-1, -1), 2);
+	cv::dilate(binImg, binImg, cv::Mat(5, 5, CV_8U), cv::Point(-1, -1), 2);
+
+	cv::morphologyEx(binImg, binImg, cv::MORPH_OPEN, cv::Mat(5, 5, CV_8U), cv::Point(-1, -1), 1);
+	cv::morphologyEx(binImg, binImg, cv::MORPH_CLOSE, cv::Mat(5, 5, CV_8U), cv::Point(-1, -1), 1);
+
+	std::vector<std::vector<cv::Point>> contours;
+	std::vector<cv::Vec4i> hierarchy;
+	cv::Mat tmpBinImg;
+	binImg.copyTo(tmpBinImg);
+	cv::findContours(tmpBinImg, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+
+	cv::drawContours(orgImg, contours, -1, cv::Scalar(0, 0,255), 2);
+	
+	cv::imshow(orgStr, orgImg);
+	cv::imshow(grayStr, grayImg);
 	cv::imshow(binStr, binImg);
 
+	
 
-	if (cv::waitKey(0) == 0x1b)
-		cv::destroyAllWindows();
+	if (cv::waitKey(0) == 0x1b)		cv::destroyAllWindows();
 
 	return 0;
 }
